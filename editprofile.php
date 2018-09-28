fe345128cf0b71bf702a82fbdf5489dcc0468e2e
Danish Sehgal <danishsehgal2010@gmail.com>
2018-09-28 20:01:51 +0530

    Manage Profile layout complete

--- editprofile.php
+++ editprofile.php
@@ -16,17 +16,7 @@ while ($row = mysqli_fetch_array($result)) {
         <small class="text-danger"> <?php echo $emailError; ?></small> 
   </div>
 <br> 
-    <div class="form-row">
-      <input type="Password" id="password" class="form-control profile-inputs" placeholder="Password" name="password" required>
-        <small class="text-danger"> <?php echo $passwordError; ?></small>
-  </div>
-            <br>
-            
-    <div class="form-row">
-      <input type="Password" id="confirm_password" class="form-control profile-inputs" placeholder="Confirm Password" name="confirmPassword" required ><span id='message'></span>
-  </div>
-        <br>    
-        
+    
             <div class="form-row">
       <input type="number" class="form-control profile-inputs" placeholder="Mobile Number" name="mobileNumber" required value="<?php echo $_SESSION['mobileNumber'];?>">
         <small class="text-danger"> <?php echo $mobileError; ?></small>
@@ -45,26 +35,7 @@ while ($row = mysqli_fetch_array($result)) {
   </div>
          <br>
   <div class="form-row">
-    <div class="col-md-2">
-      <p class="labels-for-form">Gender:</p>
-      </div>
-      <div class="col-md-10">
-      <div class="row">
-  <div class="form-check form-check-inline col-md-4">
-  <input class="form-check-input" type="radio" name="gender" id="inlineRadio1" value="Male">
-  <label class="form-check-label" for="inlineRadio1">Male</label>
-</div>
-<div class="form-check form-check-inline col-md-4">
-  <input class="form-check-input" type="radio" name="gender" id="inlineRadio2" value="Female">
-  <label class="form-check-label" for="inlineRadio2">Female</label>
-</div>
-<div class="form-check form-check-inline col-md-4">
-  <input class="form-check-input" type="radio" name="gender" id="inlineRadio3" value="Transgender">
-  <label class="form-check-label" for="inlineRadio3">Transgender</label>
-</div> 
-    <small class="text-danger"><?php echo $genderError; ?></small>
-    </div>
-    </div>      
+    <input class="form-control profile-inputs" type="text" name="gender" readonly>     
   </div>
         <br>
     <div class="form-row">
@@ -79,7 +50,7 @@ while ($row = mysqli_fetch_array($result)) {
             <br>
             
             <div class="form-row">
-    <div class="col-md-4"> 
+    <div style="padding:0" class="col-md-4"> 
         <select style="height:48px; padding:10px 18px;" name="qualification" onchange="checkSelect()" class="js-example-placeholder-single js-states form-control profile-inputs">
             <small class="text-danger"><?php echo $eduError; ?></small>
             <option></option>
@@ -92,20 +63,23 @@ while ($row = mysqli_fetch_array($result)) {
             <option></option>
         </select>
     </div>
-    <div id="selectDiv3" class="col-md-4">
+    <div style="padding:0" id="selectDiv3" class="col-md-4">
         <select style="height:48px;padding:10px 18px;" name="field" id="selectThree" class="js-example-placeholder-single js-states form-control profile-inputs">
             <small class="text-danger"><?php echo $fieldError; ?></small>
             <option></option>
         </select>
     </div>
   </div>
-          
-  <div class="form-group">
-    <div class="custom-control custom-checkbox">
-    <input type="checkbox" class="custom-control-input" id="defaultUnchecked" name="checkbox" value="yes">
-    <label class="custom-control-label" for="defaultUnchecked">I have read and agree with the Terms and Conditions. </label>
-</div>
-    <small class="text-danger"><?php echo $checkboxError; ?></small>
+            <br>
+<h1 class="heading-profile">Change Password</h1>     <div class="form-row">
+      <input type="Password" id="password" class="form-control profile-inputs" placeholder="New Password" name="password" required>
+        <small class="text-danger"> <?php echo $passwordError; ?></small>
   </div>
-  <button class="btn btn-primary" type="submit" name="add">Submit form</button> 
+            <br>
+            
+    <div class="form-row">
+      <input type="Password" id="confirm_password" class="form-control profile-inputs" placeholder="Confirm Password" name="confirmPassword" required ><span id='message'></span>
+  </div>
+        <br>    
+  <button class="save-changes" type="submit" name="add">Save Changes</button> 
 </form>
--- register.js
+++ register.js
@@ -1,8 +1,8 @@
 $( function() {
     $( "#datepicker,#datepickerProfile" ).datepicker({changeYear:true,
-                                   yearRange:'-60:',
-                                  dateFormat:'yy-mm-dd',
-                                  maxDate: "-14y" });
+                yearRange:'-60:',
+                dateFormat:'yy-mm-dd',
+                maxDate: "-14y" });
   } );
 $(".js-example-placeholder-single").select2({
     placeholder: "Select Your Qualification",
@@ -53,3 +53,20 @@ $('#password, #confirm_password').on('keyup', function () {
  $("#editProfile").click(function(){
     $('#show').load("editprofile.php" +  '#show');
         });
+
+$("#myAccount").click(function(){
+   $(this).css({"color":"#00aff0","font-weight":"600"});
+    $("#appHistory,#editProfile,#logout").css({"color":"#000","font-weight":"normal"});
+});
+$("#appHistory").click(function(){
+   $(this).css({"color":"#00aff0","font-weight":"600"});
+    $("#myAccount,#editProfile,#logout").css({"color":"#000","font-weight":"normal"});
+});
+$("#editProfile").click(function(){
+   $(this).css({"color":"#00aff0","font-weight":"600"});
+    $("#myAccount,#appHistory,#logout").css({"color":"#000","font-weight":"normal"});
+});
+$("#logout").click(function(){
+   $(this).css({"color":"#00aff0","font-weight":"600"});
+    $("#myAccount,#appHistory,#editProfile").css({"color":"#000","font-weight":"normal"});
+});
