b6a5610ed418ec7e0239f41a9954ba969d364d0f
Danish Sehgal <danishsehgal2010@gmail.com>
2018-10-15 11:23:34 +0530

    password reset

--- forgot-password.js
+++ forgot-password.js
@@ -1,7 +1,18 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 $(".send-button").click(function(){
+    var email = $('#login-username').val();
+    if(!validateEmail(email)){
+        alert("Please enter a valid email.");
+    }
+        else{
+    
     $(".forgot-password-wrapper").fadeOut(function(){
         $(".thankyou-wrapper").fadeIn();
         });
+    }
 });
 $("#resend").click(function(){
     $(".thankyou-wrapper").fadeOut(function(){
--- password-reset.php
+++ password-reset.php
@@ -18,3 +18,43 @@ function userInfo(){
 }
 }
 ?>
+<html>
+
+    <head>
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>HomePage</title>
+
+        <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="bootstrap-4.0.0/dist/css/bootstrap.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <!--        StyleSheet-->   
+        
+       <link href="https://fonts.googleapis.com/css?family=Noto+Serif+KR|Oxygen|Poppins" rel="stylesheet">
+
+    </head>
+ <body>
+     <form style="width:600px; margin: 10% auto;">
+   
+    <h1 class="page-heading" style="text-align:left; color:#333;">Reset Your Password</h1>   
+          <p class="tagline">You have requested to reset your password for .</p>
+         <div class="form-row">
+        <input type="Password" id="cur_password" class="form-control edit-profile-inputs" placeholder="New Password" name="password" required>
+        <small id="passError" class="text-danger"></small>
+      </div>
+      <br>
+
+        <div class="form-row">
+          <input type="Password" id="con_password" class="form-control edit-profile-inputs" placeholder="Confirm Password" name="confirmPassword" required ><span id='message'></span>
+            <small id="cpassError" class="text-danger"></small>
+      </div>
+            <br>    
+      <button class="save-changes" onclick="passValidation()" type="button" name="Change Password">Change Password</button>
+    <br>
+            <br><div id="resultpass"></div>
+    </form>
+</body>
+</html>
